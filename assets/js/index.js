2a37ed97994dab0f4c1d463ddb32dafe9077f8bb
Ahmad Rijal <rizal.alma95@gmail.com>
2024-01-03 17:19:23 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+const navTogglee = document.querySelector("#navTogglee");
+const navClosedIcon = document.querySelector("#navClosed");
+const navOpenIcon = document.querySelector("#navOpen");
+const navIcon = document.querySelectorAll(".navIcon");
+const nav = document.querySelector(".nav-top");
+
+navTogglee.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  navIcon.forEach((icon) => {
+    icon.classList.toggle("hidden");
+  });
+});
+
+
+window.addEventListener(
+  "resize", () => {
+    if (document.body.clientWidth > 720) {
+      nav.classList.remove("open");
+      navIcon.forEach((icon) => {
+        icon.classList.remove("hidden");
+      });
+      navOpenIcon.classList.add("hidden");
+    }
+  },
+  { passive: false }
+);
+
